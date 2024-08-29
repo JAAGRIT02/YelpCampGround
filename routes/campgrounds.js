da7ef9305efe2c869857785201e545e73d745ab6
JAAGRIT <jaagrit08@gmail.com>
2024-08-30 04:40:21 +0530

    cloudinary and multer setup

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,23 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const catchAsync = require('../Utils/catchAsync');
-const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-const campgrounds = require('../controllers/campgrounds')
-const Campground = require('../models/campground');
+const catchAsync = require("../Utils/catchAsync");
+const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const campgrounds = require("../controllers/campgrounds");
+const Campground = require("../models/campground");
+const multer = require("multer");
+const{storage}=require('../cloudinary')
+const upload = multer({ storage });
 
-router.route('/')
-    .get( catchAsync(campgrounds.index))
-    .post( isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router
+	.route("/")
+	.get(catchAsync(campgrounds.index))
+	// .post( isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+	.post(upload.array("image"), (req, res) => {         //using multer for image upload
+		console.log(req.files,req.body)
+        res.send('it worked')
+	});
 
-router.get('/new', isLoggedIn, campgrounds.renderNewForm )
+router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
+router
+	.route("/:id")
+	.get(catchAsync(campgrounds.showCampground))
+	.put(
+		isLoggedIn,
+		isAuthor,
+		validateCampground,
+		catchAsync(campgrounds.updateCampground)
+	)
+	.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-router.route('/:id')
-    .get(catchAsync(campgrounds.showCampground))
-    .put( isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
-    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
-
-
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
+router.get(
+	"/:id/edit",
+	isLoggedIn,
+	isAuthor,
+	catchAsync(campgrounds.renderEditForm)
+);
 
 module.exports = router;
