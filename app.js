a53cb028deb87511af363a0f91a625953580a64a
JAAGRIT <jaagrit08@gmail.com>
2024-09-26 12:25:55 +0530

    about to deploy

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV !== 'production'){
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
 }
 // console.log(process.env.CLOUDINARY_SECRET);
 // console.log(process.env.CLOUDINARY_KEY);
@@ -9,28 +9,27 @@ console.log(process.env.MONGO_URI);
 const express = require("express");
 const app = express();
 const path = require("path");
-const session = require('express-session')
-const flash = require('connect-flash')
+const session = require("express-session");
+const flash = require("connect-flash");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate"); //used to make boilerplate
 const methodOverride = require("method-override");
-const mongoSanitize = require('express-mongo-sanitize');  //for security
-const helmet = require('helmet') //for security
+const mongoSanitize = require("express-mongo-sanitize"); //for security
+const helmet = require("helmet"); //for security
 
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const User = require('./models/user');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const expressError = require("./Utils/Express-Error");
 
-
-const campgroundRoutes = require('./routes/campgrounds')
-const reviewRoutes = require('./routes/reviews')
-const userRoutes = require('./routes/user');
-const dbConnect  = require("./Utils/dbConnect");
-const { name } = require('ejs');
-const MongoStore = require('connect-mongo');
-const dbUrl= process.env.MONGO_URI || "mongodb://localhost:27017/yelp-camp";
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/user");
+const dbConnect = require("./Utils/dbConnect");
+const { name } = require("ejs");
+const MongoStore = require("connect-mongo");
+const dbUrl = process.env.MONGO_URI || "mongodb://localhost:27017/yelp-camp";
 // MVC-models views controllers
 
 dbConnect();
@@ -40,53 +39,55 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true })); //used to parse when creating new data
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname,'public')));
-app.use(mongoSanitize({
-  replaceWith: '_',
-}));
-app.use (helmet());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+	mongoSanitize({
+		replaceWith: "_",
+	})
+);
+app.use(helmet());
 const scriptSrcUrls = [
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://api.mapbox.com/",
-  "https://kit.fontawesome.com/",
-  "https://cdnjs.cloudflare.com/",
-  "https://cdn.jsdelivr.net",
+	"https://stackpath.bootstrapcdn.com/",
+	"https://api.tiles.mapbox.com/",
+	"https://api.mapbox.com/",
+	"https://kit.fontawesome.com/",
+	"https://cdnjs.cloudflare.com/",
+	"https://cdn.jsdelivr.net",
 ];
 const styleSrcUrls = [
-  "https://kit-free.fontawesome.com/",
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.mapbox.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://fonts.googleapis.com/",
-  "https://use.fontawesome.com/",
+	"https://kit-free.fontawesome.com/",
+	"https://stackpath.bootstrapcdn.com/",
+	"https://api.mapbox.com/",
+	"https://api.tiles.mapbox.com/",
+	"https://fonts.googleapis.com/",
+	"https://use.fontawesome.com/",
 ];
 const connectSrcUrls = [
-  "https://api.mapbox.com/",
-  "https://a.tiles.mapbox.com/",
-  "https://b.tiles.mapbox.com/",
-  "https://events.mapbox.com/",
+	"https://api.mapbox.com/",
+	"https://a.tiles.mapbox.com/",
+	"https://b.tiles.mapbox.com/",
+	"https://events.mapbox.com/",
 ];
 const fontSrcUrls = [];
 app.use(
-  helmet.contentSecurityPolicy({
-      directives: {
-          defaultSrc: [],
-          connectSrc: ["'self'", ...connectSrcUrls],
-          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-          workerSrc: ["'self'", "blob:"],
-          objectSrc: [],
-          imgSrc: [
-              "'self'",
-              "blob:",
-              "data:",
-              "https://res.cloudinary.com/dwu4dvbf8/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-              "https://images.unsplash.com/",
-          ],
-          fontSrc: ["'self'", ...fontSrcUrls],
-      },
-  })
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: ["'self'", ...connectSrcUrls],
+			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+			workerSrc: ["'self'", "blob:"],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				"blob:",
+				"data:",
+				"https://res.cloudinary.com/dwu4dvbf8/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				"https://images.unsplash.com/",
+			],
+			fontSrc: ["'self'", ...fontSrcUrls],
+		},
+	})
 );
 // const store = new MongoStore({
 //   url: dbUrl,
@@ -96,79 +97,69 @@ app.use(
 // store.on("error", function (e) {
 //   console.log("SESSION STORE ERROR", e)
 // })
-const secret=process.env.SECRET ||'thisshouldbeabettersecret!'
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
 
 const sessionConfig = {
-  name:'session',
-  secret ,
-  resave: false,
-  saveUninitialized: true, //making away deprication warning
-  
-  cookie: {
-      httpOnly: true,
-      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,  //setting expiration date on the cookie
-      maxAge: 1000 * 60 * 60 * 24 * 7
-  },
-  store: MongoStore.create({
-    mongoUrl: dbUrl,
-    touchAfter: 24 * 3600 // time period in seconds
-  })
-}
+	name: "session",
+	secret,
+	resave: false,
+	saveUninitialized: true, //making away deprication warning
 
-app.use(session(sessionConfig))
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7, //setting expiration date on the cookie
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
+	store: MongoStore.create({
+		mongoUrl: dbUrl,
+		touchAfter: 24 * 3600, // time period in seconds
+	}),
+};
+
+app.use(session(sessionConfig));
 app.use(flash());
 
 //passport for authentication
 app.use(passport.initialize());
-app.use(passport.session());  //should be below sessions
+app.use(passport.session()); //should be below sessions
 passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser());  //store in session
-passport.deserializeUser(User.deserializeUser());//unstore in session
+passport.serializeUser(User.serializeUser()); //store in session
+passport.deserializeUser(User.deserializeUser()); //unstore in session
 
 app.use((req, res, next) => {
-  // console.log(req.session)
-  res.locals.currentUser = req.user;  //to get data of the current user
-  res.locals.success = req.flash('success'); //for success
-  res.locals.error = req.flash('error');
-  next();
-})
-
-app.get('/fakeUser', async(req,res)=>{
-  const user = new User({email:'jaax@gmail.com',username:'jaax'})
-  const newUser = await User.register(user,'jaxx');  //setting password
-  res.send(newUser);
-})
-
-
-//express router
-app.use('/',userRoutes);
-app.use('/campgrounds',campgroundRoutes);
-app.use('/campgrounds/:id/reviews',reviewRoutes);
-
-
-app.get("/", (req, res) => {
-  res.render("home"); //home.ejs created
+	// console.log(req.session)
+	res.locals.currentUser = req.user; //to get data of the current user
+	res.locals.success = req.flash("success"); //for success
+	res.locals.error = req.flash("error");
+	next();
 });
 
+app.get("/fakeUser", async (req, res) => {
+	const user = new User({ email: "jaax@gmail.com", username: "jaax" });
+	const newUser = await User.register(user, "jaxx"); //setting password
+	res.send(newUser);
+});
 
+//express router
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-
+app.get("/", (req, res) => {
+	res.render("home"); //home.ejs created
+});
 
 app.all("*", (req, res, next) => {
-  next(new expressError("page not found", 404));
+	next(new expressError("page not found", 404));
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err;
-  if (!err.message) err.message = "something went wrong";
-  res.status(statusCode).render("error", { err });
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = "something went wrong";
+	res.status(statusCode).render("error", { err });
 });
-
-
 
 app.listen(3000, () => {
-  console.log("Serving on port 3000");
+	console.log("Serving on port 3000");
 });
-
-
